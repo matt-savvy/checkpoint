83ba81d536fc85b70a03ac14c8413da7119d6fa2
matt savoia <xbrucexwaynex@gmail.com>
2018-03-23 16:34:58 -0700

    only show avail racer numbers

--- racers/forms.py
+++ racers/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.db import models
 from .models import Racer
 
 class RacerForm(forms.ModelForm):
@@ -6,10 +7,23 @@ class RacerForm(forms.ModelForm):
         model = Racer
         
 class RegisterForm(forms.ModelForm):
-    racer_number = forms.IntegerField(max_value=999, widget=forms.NumberInput)
+    available_numbers = range(1, 1000)
+    available_numbers_tup = tuple([(element, element) for element in available_numbers])
+    racer_number = forms.ChoiceField(choices=available_numbers)
     
     class Meta:
         model = Racer
         fields = ('racer_number', 'first_name', 'last_name', 'nick_name', 'city', 'email', 'gender', 'category', 'team', 'company')
     
-    
+    def __init__(self, *args, **kwargs):
+        super(RegisterForm, self).__init__(*args, **kwargs)
+        racer_numbers = range(1, 1000)
+        existing_numbers = Racer.objects.values_list('racer_number', flat=True)
+        for number in existing_numbers:
+            number = int(number)
+        existing_numbers = [int(x) for x in existing_numbers]
+        available_numbers = [x for x in racer_numbers if x not in existing_numbers]
+        available_numbers_tup = tuple([(element, str(element)) for element in available_numbers])
+        self.fields['racer_number'].choices = available_numbers_tup
+        
+        
