74464938a934b80250c47c22df3bd439f077d687
Matt Savoia <xbrucexwaynex@gmail.com>
2018-09-20 22:17:05 -0400

    update auto-replace typo

--- racers/management/commands/volunteer_from_session.py
+++ racers/management/commands/volunteer_from_session.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand, CommandError
-from volunteers.models import Volunteer
+from racers.models import Volunteer
 from importlib import import_module
 from ajax.serializers import RegistrationSerializer
 from rest_framework.renderers import JSONRenderer
