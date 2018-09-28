b79dbc59c9230730aed731772d600a7797e67f41
Matt Savoia <xbrucexwaynex@gmail.com>
2018-09-27 23:47:50 -0400

    handling for finished/dq racers

--- appapi/views.py
+++ appapi/views.py
@@ -81,14 +81,22 @@ class RacerCheckpointView(APIView):
         racer = Racer.objects.filter(racer_number=racer_number).first()
         
         if racer:
-            race_entry = RaceEntry.objects.filter(racer=racer).filter(race=current_race)
+            race_entry = RaceEntry.objects.filter(racer=racer).filter(race=current_race).first()
             if race_entry:
-                available_runs = get_available_runs(race_entry, checkpoint)
-                serialized_runs = RunSerializer(available_runs)
-                serialized_racer = RacerSerializer(racer)
+                  if race_entry.entry_status == RaceEntry.ENTRY_STATUS_RACING or race_entry.entry_status == RaceEntry.ENTRY_STATUS_CUT:
+                      available_runs = get_available_runs(race_entry, checkpoint)
+                      serialized_runs = RunSerializer(available_runs)
+                      serialized_racer = RacerSerializer(racer)
             
-                return Response({'racer' : serialized_racer.data, 'runs' : serialized_runs.data, 'error' : False, 'error_title' : None, 'error_description' : None}, status=status.HTTP_200_OK)
+                      return Response({'racer' : serialized_racer.data, 'runs' : serialized_runs.data, 'error' : False, 'error_title' : None, 'error_description' : None}, status=status.HTTP_200_OK)
+            
+                  elif race_entry.entry_status == RaceEntry.ENTRY_STATUS_FINISHED or race_entry.entry_status == RaceEntry.ENTRY_STATUS_PROCESSING:
+                      return Response({'error': True, 'error_title': "Racer is finished!", "error_description": "This racer has already finished the race"}, status=status.HTTP_200_OK)
                 
+                  elif race_entry.entry_status == RaceEntry.ENTRY_STATUS_DQD:
+                      return Response({'error': True, 'error_title': "Racer has been disqualified!", "error_description": "This racer is marked as disqualified. Have them report to headquarters."}, status=status.HTTP_200_OK)
+                  
+            
         else:
             return Response({'error' : True, 'error_title' : 'Cannot Find Racer', 'error_description' : 'No racer found with racer number {}.'.format(str(racer_number))}, status=status.HTTP_200_OK)
 
