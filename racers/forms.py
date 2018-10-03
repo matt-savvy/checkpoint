342efbf00f8ffeb87f3ac985a428485992333209
matt savoia <xbrucexwaynex@gmail.com>
2018-10-03 00:47:08 -0400

    update racer form

--- racers/forms.py
+++ racers/forms.py
@@ -3,9 +3,24 @@ from django.db import models
 from .models import Racer, Volunteer
 
 class RacerForm(forms.ModelForm):
+    available_numbers = range(1, 1000)
+    available_numbers_tup = tuple([(element, element) for element in available_numbers])
+    racer_number = forms.ChoiceField(choices=available_numbers)
     class Meta:
         model = Racer
-        
+    
+    def __init__(self, *args, **kwargs):
+        super(RacerForm, self).__init__(*args, **kwargs)
+        racer_numbers = range(500, 1000)
+        existing_numbers = Racer.objects.values_list('racer_number', flat=True)
+        numbers_to_fill = 300 - len(existing_numbers)
+        for number in existing_numbers:
+            number = int(number)
+        existing_numbers = [int(x) for x in existing_numbers]
+        available_numbers = [x for x in racer_numbers if x not in existing_numbers][:numbers_to_fill]
+        available_numbers_tup = tuple([(element, str(element)) for element in available_numbers])
+        self.fields['racer_number'].choices = available_numbers_tup
+    
 class VolunteerForm(forms.ModelForm):
     class Meta:
         model = Volunteer
