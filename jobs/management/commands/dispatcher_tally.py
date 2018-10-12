5eb6ebd5ba68479c9390744d69b5268a311982d3
matt savoia <xbrucexwaynex@gmail.com>
2018-10-11 21:30:15 -0400

    update dispatch tally

--- jobs/management/commands/dispatcher_tally.py
+++ jobs/management/commands/dispatcher_tally.py
@@ -21,17 +21,50 @@ from nacccusers.models import NACCCUser
 class Command(BaseCommand):
     def handle(self, *args, **options):
         dispatchers = NACCCUser.objects.all()
-        for dispatcher in dispatchers:
-            dispatcher.tally = 0
-        
+        chris49 = NACCCUser.objects.get(pk=7)
+        chris49.tally = 0
+        dave = NACCCUser.objects.get(pk=20)
+        dave.tally = 0
+        doug = NACCCUser.objects.get(pk=12)
+        doug.tally = 0
+        hank = NACCCUser.objects.get(pk=6)
+        hank.tally = 0
+        jessie = NACCCUser.objects.get(pk=8)
+        jessie.tally = 0
+        magic = NACCCUser.objects.get(pk=4)
+        magic.tally = 0
+        matt = NACCCUser.objects.get(pk=1)
+        matt.tally = 0
+        baj = NACCCUser.objects.get(pk=2)
+        baj.tally = 0
+        dispatchers = [chris49, dave, doug, hank, jessie, magic, matt, baj]
         logs = RaceLog.objects.filter(race__pk=2).filter(log__contains="Racer confirmed message ")
         for log in logs:
             pk = log.log.split("Racer confirmed message ")[1]
             pk = pk.split(".")[0]
             message = Message.objects.filter(pk=pk).first()
             if message:
-                dispatcher = log.user
                 run_count = message.runs.count()
+                dispatcher = log.user
+                if dispatcher == chris49:
+                    chris49.tally += run_count
+                elif dispatcher == dave:
+                    dave.tally += run_count
+                elif dispatcher == doug:
+                    doug.tally += run_count
+                elif dispatcher == hank:
+                    hank.tally += run_count
+                elif dispatcher == jessie:
+                    jessie.tally += run_count
+                elif dispatcher == magic:
+                    magic.tally += run_count
+                elif dispatcher == magic:
+                    magic.tally += run_count
+                elif dispatcher == matt:
+                    matt.tally += run_count
+                elif dispatcher == baj:
+                    baj.tally += run_count
+                
                 dispatcher.tally =+ run_count
 
         for dispatcher in dispatchers:
