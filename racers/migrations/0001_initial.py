26034c6c5b6664e9e7405d90003479c12f91f7c7
Matt Savoia <xbrucexwaynex@gmail.com>
2019-05-08 00:46:13 -0400

    Squash racers migrations

--- racers/migrations/0001_initial.py
+++ racers/migrations/0001_initial.py
@@ -11,35 +11,141 @@ class Migration(SchemaMigration):
         # Adding model 'Racer'
         db.create_table(u'racers_racer', (
             (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('racer_number', self.gf('django.db.models.fields.IntegerField')()),
+            ('company', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['companies.Company'])),
+            ('racer_number', self.gf('django.db.models.fields.CharField')(unique=True, max_length=3)),
             ('first_name', self.gf('django.db.models.fields.CharField')(max_length=50)),
             ('last_name', self.gf('django.db.models.fields.CharField')(max_length=50)),
             ('nick_name', self.gf('django.db.models.fields.CharField')(max_length=50, blank=True)),
+            ('email', self.gf('django.db.models.fields.EmailField')(max_length=50, blank=True)),
             ('city', self.gf('django.db.models.fields.CharField')(max_length=50, blank=True)),
-            ('country', self.gf('django.db.models.fields.CharField')(max_length=50)),
             ('gender', self.gf('django.db.models.fields.CharField')(max_length=1)),
             ('category', self.gf('django.db.models.fields.IntegerField')()),
+            ('track', self.gf('django.db.models.fields.BooleanField')(default=False)),
+            ('cargo', self.gf('django.db.models.fields.BooleanField')(default=False)),
+            ('packet', self.gf('django.db.models.fields.BooleanField')(default=False)),
+            ('heat', self.gf('django.db.models.fields.CharField')(default='a', max_length=2)),
+            ('shirt_size', self.gf('django.db.models.fields.CharField')(default='M', max_length=2)),
+            ('paid', self.gf('django.db.models.fields.BooleanField')(default=False)),
+            ('paypal_tx', self.gf('django.db.models.fields.CharField')(max_length=100, blank=True)),
+            ('team', self.gf('django.db.models.fields.CharField')(max_length=100, blank=True)),
+            ('radio_number', self.gf('django.db.models.fields.CharField')(max_length=100, blank=True)),
+            ('contact_info', self.gf('django.db.models.fields.CharField')(max_length=100, blank=True)),
         ))
         db.send_create_signal(u'racers', ['Racer'])
+
+        # Adding model 'Volunteer'
+        db.create_table(u'racers_volunteer', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('first_name', self.gf('django.db.models.fields.CharField')(max_length=50)),
+            ('last_name', self.gf('django.db.models.fields.CharField')(max_length=50)),
+            ('email', self.gf('django.db.models.fields.EmailField')(max_length=50, blank=True)),
+            ('phone', self.gf('django.db.models.fields.CharField')(max_length=15)),
+            ('city', self.gf('django.db.models.fields.CharField')(max_length=50, blank=True)),
+            ('shirt_size', self.gf('django.db.models.fields.CharField')(default='M', max_length=2)),
+            ('paid', self.gf('django.db.models.fields.BooleanField')(default=False)),
+            ('paypal_tx', self.gf('django.db.models.fields.CharField')(max_length=100, blank=True)),
+            ('packet_picked_up', self.gf('django.db.models.fields.BooleanField')(default=False)),
+        ))
+        db.send_create_signal(u'racers', ['Volunteer'])
 
 
     def backwards(self, orm):
         # Deleting model 'Racer'
         db.delete_table(u'racers_racer')
 
+        # Deleting model 'Volunteer'
+        db.delete_table(u'racers_volunteer')
+
 
     models = {
+        u'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        u'auth.permission': {
+            'Meta': {'ordering': "(u'content_type__app_label', u'content_type__model', u'codename')", 'unique_together': "((u'content_type', u'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        u'checkpoints.checkpoint': {
+            'Meta': {'ordering': "['checkpoint_number']", 'object_name': 'Checkpoint'},
+            'address_line_1': ('django.db.models.fields.CharField', [], {'max_length': '144', 'null': 'True', 'blank': 'True'}),
+            'address_line_2': ('django.db.models.fields.CharField', [], {'max_length': '144', 'null': 'True', 'blank': 'True'}),
+            'checkpoint_name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'checkpoint_number': ('django.db.models.fields.IntegerField', [], {'unique': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'notes': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'open_late': ('django.db.models.fields.BooleanField', [], {'default': 'False'})
+        },
+        u'companies.company': {
+            'Meta': {'object_name': 'Company'},
+            'dispatcher': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['nacccusers.NACCCUser']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        },
+        u'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        u'nacccusers.nacccuser': {
+            'Meta': {'object_name': 'NACCCUser'},
+            'authorized_checkpoints': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['checkpoints.Checkpoint']", 'symmetrical': 'False', 'blank': 'True'}),
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Group']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Permission']"}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
         u'racers.racer': {
-            'Meta': {'object_name': 'Racer'},
+            'Meta': {'ordering': "['last_name']", 'object_name': 'Racer'},
+            'cargo': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
             'category': ('django.db.models.fields.IntegerField', [], {}),
             'city': ('django.db.models.fields.CharField', [], {'max_length': '50', 'blank': 'True'}),
-            'country': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'company': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['companies.Company']"}),
+            'contact_info': ('django.db.models.fields.CharField', [], {'max_length': '100', 'blank': 'True'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '50', 'blank': 'True'}),
             'first_name': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
             'gender': ('django.db.models.fields.CharField', [], {'max_length': '1'}),
+            'heat': ('django.db.models.fields.CharField', [], {'default': "'a'", 'max_length': '2'}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'last_name': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
             'nick_name': ('django.db.models.fields.CharField', [], {'max_length': '50', 'blank': 'True'}),
-            'racer_number': ('django.db.models.fields.IntegerField', [], {})
+            'packet': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'paid': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'paypal_tx': ('django.db.models.fields.CharField', [], {'max_length': '100', 'blank': 'True'}),
+            'racer_number': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '3'}),
+            'radio_number': ('django.db.models.fields.CharField', [], {'max_length': '100', 'blank': 'True'}),
+            'shirt_size': ('django.db.models.fields.CharField', [], {'default': "'M'", 'max_length': '2'}),
+            'team': ('django.db.models.fields.CharField', [], {'max_length': '100', 'blank': 'True'}),
+            'track': ('django.db.models.fields.BooleanField', [], {'default': 'False'})
+        },
+        u'racers.volunteer': {
+            'Meta': {'ordering': "['last_name']", 'object_name': 'Volunteer'},
+            'city': ('django.db.models.fields.CharField', [], {'max_length': '50', 'blank': 'True'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '50', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'packet_picked_up': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'paid': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'paypal_tx': ('django.db.models.fields.CharField', [], {'max_length': '100', 'blank': 'True'}),
+            'phone': ('django.db.models.fields.CharField', [], {'max_length': '15'}),
+            'shirt_size': ('django.db.models.fields.CharField', [], {'default': "'M'", 'max_length': '2'})
         }
     }
 
